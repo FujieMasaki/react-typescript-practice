240590bc704daa39e2caf0d5e18561fe4fcd4dc3
Masaki Fujie <fujiemasaki@fujiemasakinoMacBook-Air.local>
2022-03-29 09:09:31 +0900

    propsの型定義

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,4 +1,13 @@
-export const Todo = (props) => {
-    const { title, userid } = props;
-    return <p>{`${title}(ユーザー:${userid})`}</p>
+type TodoType = {
+  userId: number;
+  title: string;
+  completed?: boolean;
+//   あってもなくても良いpropsは?をつける
+};
+
+export const Todo = (props: TodoType) => {
+  const { title, userId, completed = false } = props;
+//   typeを?にするときは初期値を設定
+  const completedMark = completed ? "[完]" : "[未]";
+  return <p>{`${completedMark}${title}(ユーザー:${userId})`}</p>;
 };
